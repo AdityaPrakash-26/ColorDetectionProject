4ed626ebf755965e237ccc1e2138a84f257e870b
Aditya <prakashadityaap@gmail.com>
2021-04-02 18:21:29 +0530

    Added light image colour compatibility

--- main.py
+++ main.py
@@ -63,6 +63,10 @@ while 1:
         # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
         cv2.putText(img, colorName, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
 
+        # For very light colours we will display text in black colour
+        if (r + g + b >= 600):
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+
         clicked = False
 
     # Exit Program
