7acd850eb21960a1c4e52a18a3140c0cd4178136
Aditya Prakash <55011564+AdityaPrakash-26@users.noreply.github.com>
2021-04-03 00:40:35 +0530

    Optimised code

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import cv2
-import numpy as np
-import pandas as pd
-import argparse
-import sys
+import cv2  # image loading and displaying
+import pandas as pd  # quick calculation
+import argparse  # taking arguments
 
 # Creating argument parser to take image path from command line
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True, help="Image Path")
+ap.add_argument("-i", "--image", required=True, help="Image Path")
 args = vars(ap.parse_args())
-img_path = args['image']
+img_path = args["image"]
 
 # Reading the image with opencv
 img = cv2.imread(img_path)
+height, width, _ = img.shape
+
+# square root is the actual max but that requires importing the math module
+max_distance = height * width
+
 print("Press Esc to exit the program!  :)")
 
 # declaring global variables (are used later on)
@@ -20,14 +23,18 @@ r = g = b = xpos = ypos = 0
 
 # Reading csv file with pandas and giving names to each column
 index = ["color", "color_name", "hex", "R", "G", "B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
+csv = pd.read_csv("colors.csv", names=index, header=None)
 
 
 # function to calculate minimum distance from all colors and get the most matching color
 def getColorName(R, G, B):
-    minimum = sys.maxsize
+    minimum = max_distance
     for i in range(len(csv)):
-        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        d = (
+            abs(R - int(csv.loc[i, "R"]))
+            + abs(G - int(csv.loc[i, "G"]))
+            + abs(B - int(csv.loc[i, "B"]))
+        )
         if d <= minimum:
             minimum = d
             cname = csv.loc[i, "color_name"]
@@ -39,7 +46,6 @@ def draw_function(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         global b, g, r, xpos, ypos, clicked
         clicked = True
-        print("clicked!")
         xpos = x
         ypos = y
         b, g, r = img[y, x]
@@ -48,29 +54,22 @@ def draw_function(event, x, y, flags, param):
         r = int(r)
 
 
-cv2.namedWindow('Photo')
-cv2.setMouseCallback('Photo', draw_function)
+cv2.namedWindow("Photo")
+cv2.setMouseCallback("Photo", draw_function)
 
-while 1:
-
+while True:
     cv2.imshow("Photo", img)
     if clicked:
-        # image, startpoint, endpoint, color, thickness)-1 fills entire rectangle
         cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
-
-        # Creating text string to display( Color name and RGB values )
         colorName = getColorName(r, g, b) + " R=" + str(r) + " G=" + str(g) + " B=" + str(b)
-
-        # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
         cv2.putText(img, colorName, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
 
-        # For very light colours we will display text in black colour
-        if (r + g + b >= 600):
+        if r + g + b >= 600:
             cv2.putText(img, colorName, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
 
         clicked = False
 
-    # Exit Program
+    # Exit Program if esc key is pressed
     if cv2.waitKey(20) & 0xFF == 27:
         break
 
